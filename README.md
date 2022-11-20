afb77f16a60447a3ba23f971368f679494edeabd
Fakhreddine Youssef <81659132+fakhrii-ux@users.noreply.github.com>
2022-11-20 02:22:38 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# M4L - Med For life
+
+A Flutter App for online consultations to faciltate the contact between the doctor & the patient.
+
+Theme : Telemedicine
+Team : Debug Entity
+Figma Link : https://www.figma.com/file/M6CdPm7UJ4MrZFMVn8nOEU/M4L?node-id=0%3A1
+Compiled with online Compiler : https://flutlab.io/
+
+You can consult the pdf file or the presentation file for more infos.
+
+https://github.com/fakhrii-ux
---
+++ lib/main.dart
@@ -0,0 +1,223 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/telemedicineapp/generatedwelcomewidget/GeneratedWelcomeWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatedregisterwidget/GeneratedRegisterWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatedlogininwidget/GeneratedLogininWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatedhomewidget1/GeneratedHomeWidget1.dart';
+import 'package:flutterapp/telemedicineapp/generatedhomewidget3/GeneratedHomeWidget3.dart';
+import 'package:flutterapp/telemedicineapp/generatedconsultationwidget/GeneratedConsultationWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatednotificationswidget/GeneratedNotificationsWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatedconsultationwidget1/GeneratedConsultationWidget1.dart';
+import 'package:flutterapp/telemedicineapp/generateduser_interfacewidget/GeneratedUser_interfaceWidget.dart';
+import 'package:flutterapp/telemedicineapp/generateddoctor_interfacewidget/GeneratedDoctor_interfaceWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatedproblemswidget/GeneratedProblemsWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatedadviceswidget/GeneratedAdvicesWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatedforget_passwordwidget/GeneratedForget_passwordWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatedreset_passwordwidget/GeneratedReset_passwordWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatedsearch_doctorwidget/GeneratedSearch_doctorWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatedsearch_doctorwidget1/GeneratedSearch_doctorWidget1.dart';
+import 'package:flutterapp/telemedicineapp/generatedcode_verificationwidget/GeneratedCode_verificationWidget.dart';
+import 'package:flutterapp/telemedicineapp/generateduser_settingswidget/GeneratedUser_settingsWidget.dart';
+import 'package:flutterapp/telemedicineapp/generateddoctor_commentswidget/GeneratedDoctor_commentsWidget.dart';
+import 'package:flutterapp/telemedicineapp/generateddoctor_consultationswidget/GeneratedDoctor_consultationsWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatedadvicesotherwidget/GeneratedAdvicesotherWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatedmessagecirclewidget/GeneratedMessagecircleWidget.dart';
+import 'package:flutterapp/telemedicineapp/generateddoctor_settingswidget/GeneratedDoctor_settingsWidget.dart';
+import 'package:flutterapp/telemedicineapp/generateddoctor_datawidget/GeneratedDoctor_dataWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatedsearchwidget1/GeneratedSearchWidget1.dart';
+import 'package:flutterapp/telemedicineapp/generateduser_datawidget/GeneratedUser_dataWidget.dart';
+import 'package:flutterapp/telemedicineapp/generateduser_userwidget/GeneratedUser_userWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatedappointementwidget/GeneratedAppointementWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatedappointementwidget1/GeneratedAppointementWidget1.dart';
+import 'package:flutterapp/telemedicineapp/generateddonation_partwidget/GeneratedDonation_partWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatedhelpwidget2/GeneratedHelpWidget2.dart';
+import 'package:flutterapp/telemedicineapp/generatedfaqwidget1/GeneratedFAQWidget1.dart';
+import 'package:flutterapp/telemedicineapp/generatedcontact_uswidget/GeneratedContact_usWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatedterms_conditionwidget/GeneratedTerms_ConditionWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatedprivacy_policywidget/GeneratedPrivacy_PolicyWidget.dart';
+import 'package:flutterapp/telemedicineapp/generateduserwidget1/GeneratedUserWidget1.dart';
+import 'package:flutterapp/telemedicineapp/generatedbellwidget1/GeneratedBellWidget1.dart';
+import 'package:flutterapp/telemedicineapp/generatedterminalwidget1/GeneratedTerminalWidget1.dart';
+import 'package:flutterapp/telemedicineapp/generatedhelpcirclewidget1/GeneratedHelpcircleWidget1.dart';
+import 'package:flutterapp/telemedicineapp/generatedlogoutwidget3/GeneratedLogoutWidget3.dart';
+import 'package:flutterapp/telemedicineapp/generatedcodesandboxwidget1/GeneratedCodesandboxWidget1.dart';
+import 'package:flutterapp/telemedicineapp/generatedluxurygirlwidget/GeneratedLuxuryGirlWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatedappointementwidget2/GeneratedAppointementWidget2.dart';
+import 'package:flutterapp/telemedicineapp/generatedappointementwidget3/GeneratedAppointementWidget3.dart';
+import 'package:flutterapp/telemedicineapp/generatedappointementwidget4/GeneratedAppointementWidget4.dart';
+import 'package:flutterapp/telemedicineapp/generatedappointementwidget5/GeneratedAppointementWidget5.dart';
+import 'package:flutterapp/telemedicineapp/generatedpaymentwidget/GeneratedPaymentWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatedpaymentwidget2/GeneratedPaymentWidget2.dart';
+import 'package:flutterapp/telemedicineapp/generatedterms_conditionwidget1/GeneratedTerms_ConditionWidget1.dart';
+import 'package:flutterapp/telemedicineapp/generatedmemershipwidget/GeneratedMemershipWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatedbookopenwidget/GeneratedBookopenWidget.dart';
+import 'package:flutterapp/telemedicineapp/generatedwidget3/GeneratedWidget3.dart';
+import 'package:flutterapp/telemedicineapp/generatedwidget4/GeneratedWidget4.dart';
+import 'package:flutterapp/telemedicineapp/generatedrectangle10widget4/GeneratedRectangle10Widget4.dart';
+import 'package:flutterapp/telemedicineapp/generatedrectangle20widget1/GeneratedRectangle20Widget1.dart';
+import 'package:flutterapp/telemedicineapp/generatedpastwidget3/GeneratedPastWidget3.dart';
+import 'package:flutterapp/telemedicineapp/generatedrectangle9widget2/GeneratedRectangle9Widget2.dart';
+import 'package:flutterapp/telemedicineapp/generatedsearchwidget3/GeneratedSearchWidget3.dart';
+import 'package:flutterapp/telemedicineapp/generatedvectorwidget849/GeneratedVectorWidget849.dart';
+import 'package:flutterapp/telemedicineapp/generatedvectorwidget850/GeneratedVectorWidget850.dart';
+import 'package:flutterapp/telemedicineapp/generatedyesterdaymarch192022widget/GeneratedYesterdayMarch192022Widget.dart';
+import 'package:flutterapp/telemedicineapp/generatedyesterdaymarch142022widget/GeneratedYesterdayMarch142022Widget.dart';
+import 'package:flutterapp/telemedicineapp/generatedrectangle13widget7/GeneratedRectangle13Widget7.dart';
+import 'package:flutterapp/telemedicineapp/generatedrectangle15widget4/GeneratedRectangle15Widget4.dart';
+import 'package:flutterapp/telemedicineapp/generatedrectangle14widget5/GeneratedRectangle14Widget5.dart';
+import 'package:flutterapp/telemedicineapp/generatedrectangle12widget6/GeneratedRectangle12Widget6.dart';
+import 'package:flutterapp/telemedicineapp/generatedrectangle16widget4/GeneratedRectangle16Widget4.dart';
+import 'package:flutterapp/telemedicineapp/generatedrectangle18widget3/GeneratedRectangle18Widget3.dart';
+import 'package:flutterapp/telemedicineapp/generatedarousachrafwidget/GeneratedArousAchrafWidget.dart';
+import 'package:flutterapp/telemedicineapp/generated12001230pmwidget4/Generated12001230PMWidget4.dart';
+import 'package:flutterapp/telemedicineapp/generatedviewwidget6/GeneratedViewWidget6.dart';
+import 'package:flutterapp/telemedicineapp/generatedviewwidget7/GeneratedViewWidget7.dart';
+import 'package:flutterapp/telemedicineapp/generatedviewwidget8/GeneratedViewWidget8.dart';
+import 'package:flutterapp/telemedicineapp/generatedezzedinemhamdiwidget/GeneratedEzzedineMhamdiWidget.dart';
+import 'package:flutterapp/telemedicineapp/generated12001230pmwidget5/Generated12001230PMWidget5.dart';
+import 'package:flutterapp/telemedicineapp/generatedmaryemaloulouwidget/GeneratedMaryemAloulouWidget.dart';
+import 'package:flutterapp/telemedicineapp/generated12001230pmwidget6/Generated12001230PMWidget6.dart';
+import 'package:flutterapp/telemedicineapp/generatedrectangle11widget9/GeneratedRectangle11Widget9.dart';
+
+void main() {
+  runApp(TelemedicineApp());
+}
+
+class TelemedicineApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedWelcomeWidget',
+      routes: {
+        '/GeneratedWelcomeWidget': (context) => GeneratedWelcomeWidget(),
+        '/GeneratedRegisterWidget': (context) => GeneratedRegisterWidget(),
+        '/GeneratedLogininWidget': (context) => GeneratedLogininWidget(),
+        '/GeneratedHomeWidget1': (context) => GeneratedHomeWidget1(),
+        '/GeneratedHomeWidget3': (context) => GeneratedHomeWidget3(),
+        '/GeneratedConsultationWidget': (context) =>
+            GeneratedConsultationWidget(),
+        '/GeneratedNotificationsWidget': (context) =>
+            GeneratedNotificationsWidget(),
+        '/GeneratedConsultationWidget1': (context) =>
+            GeneratedConsultationWidget1(),
+        '/GeneratedUser_interfaceWidget': (context) =>
+            GeneratedUser_interfaceWidget(),
+        '/GeneratedDoctor_interfaceWidget': (context) =>
+            GeneratedDoctor_interfaceWidget(),
+        '/GeneratedProblemsWidget': (context) => GeneratedProblemsWidget(),
+        '/GeneratedAdvicesWidget': (context) => GeneratedAdvicesWidget(),
+        '/GeneratedForget_passwordWidget': (context) =>
+            GeneratedForget_passwordWidget(),
+        '/GeneratedReset_passwordWidget': (context) =>
+            GeneratedReset_passwordWidget(),
+        '/GeneratedSearch_doctorWidget': (context) =>
+            GeneratedSearch_doctorWidget(),
+        '/GeneratedSearch_doctorWidget1': (context) =>
+            GeneratedSearch_doctorWidget1(),
+        '/GeneratedCode_verificationWidget': (context) =>
+            GeneratedCode_verificationWidget(),
+        '/GeneratedUser_settingsWidget': (context) =>
+            GeneratedUser_settingsWidget(),
+        '/GeneratedDoctor_commentsWidget': (context) =>
+            GeneratedDoctor_commentsWidget(),
+        '/GeneratedDoctor_consultationsWidget': (context) =>
+            GeneratedDoctor_consultationsWidget(),
+        '/GeneratedAdvicesotherWidget': (context) =>
+            GeneratedAdvicesotherWidget(),
+        '/GeneratedMessagecircleWidget': (context) =>
+            GeneratedMessagecircleWidget(),
+        '/GeneratedDoctor_settingsWidget': (context) =>
+            GeneratedDoctor_settingsWidget(),
+        '/GeneratedDoctor_dataWidget': (context) =>
+            GeneratedDoctor_dataWidget(),
+        '/GeneratedSearchWidget1': (context) => GeneratedSearchWidget1(),
+        '/GeneratedUser_dataWidget': (context) => GeneratedUser_dataWidget(),
+        '/GeneratedUser_userWidget': (context) => GeneratedUser_userWidget(),
+        '/GeneratedAppointementWidget': (context) =>
+            GeneratedAppointementWidget(),
+        '/GeneratedAppointementWidget1': (context) =>
+            GeneratedAppointementWidget1(),
+        '/GeneratedDonation_partWidget': (context) =>
+            GeneratedDonation_partWidget(),
+        '/GeneratedHelpWidget2': (context) => GeneratedHelpWidget2(),
+        '/GeneratedFAQWidget1': (context) => GeneratedFAQWidget1(),
+        '/GeneratedContact_usWidget': (context) => GeneratedContact_usWidget(),
+        '/GeneratedTerms_ConditionWidget': (context) =>
+            GeneratedTerms_ConditionWidget(),
+        '/GeneratedPrivacy_PolicyWidget': (context) =>
+            GeneratedPrivacy_PolicyWidget(),
+        '/GeneratedUserWidget1': (context) => GeneratedUserWidget1(),
+        '/GeneratedBellWidget1': (context) => GeneratedBellWidget1(),
+        '/GeneratedTerminalWidget1': (context) => GeneratedTerminalWidget1(),
+        '/GeneratedHelpcircleWidget1': (context) =>
+            GeneratedHelpcircleWidget1(),
+        '/GeneratedLogoutWidget3': (context) => GeneratedLogoutWidget3(),
+        '/GeneratedCodesandboxWidget1': (context) =>
+            GeneratedCodesandboxWidget1(),
+        '/GeneratedLuxuryGirlWidget': (context) => GeneratedLuxuryGirlWidget(),
+        '/GeneratedAppointementWidget2': (context) =>
+            GeneratedAppointementWidget2(),
+        '/GeneratedAppointementWidget3': (context) =>
+            GeneratedAppointementWidget3(),
+        '/GeneratedAppointementWidget4': (context) =>
+            GeneratedAppointementWidget4(),
+        '/GeneratedAppointementWidget5': (context) =>
+            GeneratedAppointementWidget5(),
+        '/GeneratedPaymentWidget': (context) => GeneratedPaymentWidget(),
+        '/GeneratedPaymentWidget2': (context) => GeneratedPaymentWidget2(),
+        '/GeneratedTerms_ConditionWidget1': (context) =>
+            GeneratedTerms_ConditionWidget1(),
+        '/GeneratedMemershipWidget': (context) => GeneratedMemershipWidget(),
+        '/GeneratedBookopenWidget': (context) => GeneratedBookopenWidget(),
+        '/GeneratedWidget3': (context) => GeneratedWidget3(),
+        '/GeneratedWidget4': (context) => GeneratedWidget4(),
+        '/GeneratedRectangle10Widget4': (context) =>
+            GeneratedRectangle10Widget4(),
+        '/GeneratedRectangle20Widget1': (context) =>
+            GeneratedRectangle20Widget1(),
+        '/GeneratedPastWidget3': (context) => GeneratedPastWidget3(),
+        '/GeneratedRectangle9Widget2': (context) =>
+            GeneratedRectangle9Widget2(),
+        '/GeneratedSearchWidget3': (context) => GeneratedSearchWidget3(),
+        '/GeneratedVectorWidget849': (context) => GeneratedVectorWidget849(),
+        '/GeneratedVectorWidget850': (context) => GeneratedVectorWidget850(),
+        '/GeneratedYesterdayMarch192022Widget': (context) =>
+            GeneratedYesterdayMarch192022Widget(),
+        '/GeneratedYesterdayMarch142022Widget': (context) =>
+            GeneratedYesterdayMarch142022Widget(),
+        '/GeneratedRectangle13Widget7': (context) =>
+            GeneratedRectangle13Widget7(),
+        '/GeneratedRectangle15Widget4': (context) =>
+            GeneratedRectangle15Widget4(),
+        '/GeneratedRectangle14Widget5': (context) =>
+            GeneratedRectangle14Widget5(),
+        '/GeneratedRectangle12Widget6': (context) =>
+            GeneratedRectangle12Widget6(),
+        '/GeneratedRectangle16Widget4': (context) =>
+            GeneratedRectangle16Widget4(),
+        '/GeneratedRectangle18Widget3': (context) =>
+            GeneratedRectangle18Widget3(),
+        '/GeneratedArousAchrafWidget': (context) =>
+            GeneratedArousAchrafWidget(),
+        '/Generated12001230PMWidget4': (context) =>
+            Generated12001230PMWidget4(),
+        '/GeneratedViewWidget6': (context) => GeneratedViewWidget6(),
+        '/GeneratedViewWidget7': (context) => GeneratedViewWidget7(),
+        '/GeneratedViewWidget8': (context) => GeneratedViewWidget8(),
+        '/GeneratedEzzedineMhamdiWidget': (context) =>
+            GeneratedEzzedineMhamdiWidget(),
+        '/Generated12001230PMWidget5': (context) =>
+            Generated12001230PMWidget5(),
+        '/GeneratedMaryemAloulouWidget': (context) =>
+            GeneratedMaryemAloulouWidget(),
+        '/Generated12001230PMWidget6': (context) =>
+            Generated12001230PMWidget6(),
+        '/GeneratedRectangle11Widget9': (context) =>
+            GeneratedRectangle11Widget9(),
+      },
+    );
+  }
+}
